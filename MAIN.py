63d44d9704284a267d3f9e540573fb5712913c3c
Yinhao Li <liyinhao0413@gmail.com>
2019-05-12 21:29:07 +0800

    finish getDisparity naive

--- MAIN.py
+++ MAIN.py
@@ -4,12 +4,69 @@ from matplotlib import pyplot as plt
 
 
 
-IMGL_PATH = './images/a.jpg'
-IMGR_PATH = './images/b.jpg'
+IMGL_PATH = './images/c.jpeg'
+IMGR_PATH = './images/d.jpeg'
+WIN_SIZE = 7
+DSR = 30
 
 class main():
     def __init__(self):
-        self.img_l = cv.imread(IMGL_PATH)
-        self.img_r = cv.imread(IMGR_PATH)
+        self.img_l = cv.imread(IMGL_PATH,0)
+        self.img_r = cv.imread(IMGR_PATH,0)
 
 
+    def subKernel(self,kernel_l,kernel_r):
+        diff = np.abs(kernel_l - kernel_r)
+        return np.sum(diff)
+
+
+
+
+
+    def getDisparity(self,img_l,img_r):
+        img_height = img_l.shape[0]
+        img_width = img_l.shape[1]
+        disparity = np.zeros((img_height,img_width))
+        print(disparity.shape)
+        for i in range(img_height - WIN_SIZE):
+            for j in range(img_height - WIN_SIZE):
+                left = img_l[i : i + WIN_SIZE,j : j + WIN_SIZE]
+
+                cand = []
+                for k in range(DSR):
+                    y = j - k
+
+                    if y >= 0:
+                        right = img_r[i : i + WIN_SIZE,y: y + WIN_SIZE]
+                        val = self.subKernel(left,right)
+                        cand.append(val)
+                loc = cand.index(min(cand))
+
+                disparity[i,j] = loc * 16
+
+        print(disparity)
+
+
+
+        # for i in range(img_height - WIN_SIZE):
+        #     for j in range(img_width - WIN_SIZE):
+        #         left = img_l[i : i + WIN_SIZE,j : j + WIN_SIZE]
+        #
+        #         cand = []
+        #         for k in range(DSR):
+        #             right =
+
+
+
+
+
+
+    def mainMethod(self):
+        self.getDisparity(self.img_l,self.img_r)
+
+
+
+
+if __name__ == '__main__':
+    t = main()
+    t.mainMethod()
--- OPENCV METHOD.py
+++ OPENCV METHOD.py
@@ -2,13 +2,15 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-IMGL_PATH = './images/a.jpg'
-IMGR_PATH = './images/b.jpg'
+IMGL_PATH = './images/c.jpeg'
+IMGR_PATH = './images/d.jpeg'
 
 
-imgL = cv2.imread(IMGL_PATH)
-imgR = cv2.imread(IMGR_PATH)
-
+imgL = cv2.imread(IMGL_PATH,0)
+imgR = cv2.imread(IMGR_PATH,0)
+print(imgL.shape)
+# imgL = cv2.resize(imgL,(imgL.shape[0] // 4,imgL.shape[1] // 4),interpolation=cv2.INTER_CUBIC)
+# imgR = cv2.resize(imgR,(imgR.shape[0] // 4,imgR.shape[1] // 4),interpolation=cv2.INTER_CUBIC)
 # disparity range tuning
 window_size = 3
 min_disp = 0
@@ -28,6 +30,54 @@ stereo = cv2.StereoSGBM_create(
     preFilterCap=63,
     mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
 )
+
+# print(imgL.shape)
+
+def show(disparity):
+
+    gray = np.array(disparity)
+    output = np.zeros([gray.shape[0],gray.shape[1],3])
+    print(disparity)
+    for row in range(gray.shape[0]):
+        for col in range(gray.shape[1]):
+            color = gray[row][col]
+            if color <= 51:
+                output[row][col][0] = 255
+                output[row][col][1] = color * 5
+                continue
+
+            if color <= 102:
+                color -= 51
+                output[row][col][0] = 255 - color * 5
+                output[row][col][1] = 255
+                continue
+
+            if gray[row][col] <= 153:
+                color -= 102
+                output[row][col][1] = 255
+                output[row][col][2] = color * 5
+                continue
+
+
+
+            if gray[row][col] <= 204:
+                color -= 153
+                output[row][col][1] = 255 - int(128.0 * color / 51.0 + 0.5)
+                output[row][col][2] = 255
+                continue
+
+            else:
+                color -= 204
+                output[row][col][1] = 127 - int(128.0 * color / 51.0 + 0.5)
+                output[row][col][2] = 255
+            # output[row][col][0] = abs(255 - color)
+            # output[row][col][1] = abs(127 - color)
+            # output[row][col][2] = abs(color)
+
+    cv2.imshow("test",output)
+    cv2.waitKey()
+
 disparity = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
-plt.imshow(disparity, 'gray')
-plt.show()
+show(disparity)
+# plt.imshow(disparity, 'gray')
+# plt.show()
